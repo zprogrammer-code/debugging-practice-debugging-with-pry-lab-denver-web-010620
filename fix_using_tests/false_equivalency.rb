5e0eb4e5563a2b27300be33ee209c837c0623a98
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-20 02:41:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num = 1
-    puts "YUM YUM MUNCH MUNCH MUNCH"
+    return "YUM YUM MUNCH MUNCH MUNCH"
   else if num = 2
-    puts "HAM HAM HAM IN MY TUMMY"
+    return "HAM HAM HAM IN MY TUMMY"
   end
 end
 end
