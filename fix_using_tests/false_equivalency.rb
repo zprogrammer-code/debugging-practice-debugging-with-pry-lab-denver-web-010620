0c81713feae9c9ecc8ce5a81a79b2e9650f775e4
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-20 02:44:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,8 +13,9 @@ end
 def selection(num)
   if num == 1
     return "YUM YUM MUNCH MUNCH MUNCH"
-  els num == 2
+  else if num == 2
     return "HAM HAM HAM IN MY TUMMY"
+  end
 end
 end
 #binding.pry
